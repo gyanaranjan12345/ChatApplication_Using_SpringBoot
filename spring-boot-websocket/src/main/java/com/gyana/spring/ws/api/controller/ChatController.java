e9ee1be9ecfe2c4267129c952dadec18980566e0
Jnanranjan Behera <105503589+gyanaranjan12345@users.noreply.github.com>
2022-09-26 02:12:11 +0530

    Add files via upload

---
+++ spring-boot-websocket/src/main/java/com/gyana/spring/ws/api/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.gyana.spring.ws.api.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.gyana.spring.ws.api.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+
+}
